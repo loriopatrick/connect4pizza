4f9a33bcc551c23334be513e86fd4deffc3ee5aa
Ehimare Okoyomon <eokoyomon@berkeley.edu>
2016-11-12 04:50:25 -0800

    facebook login sending to backend

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 import './App.css';
 
 var WS = window.WS = new window.WebSocket('ws://localhost:8080');
 
 class App extends Component {
+  onClick() {
+
+  }
+  onCallback(response) {
+    console.log(response);
+    WS.send(JSON.stringify({ type: 'login', access: response.accessToken }));
+  }
   render() {
     return (
+        <div>
         <div>Hello World</div>
+        <FacebookLogin
+          appId="915076568593763"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={this.onClick.bind(this)}
+          callback={this.onCallback.bind(this)} />
+        </div>
     );
   }
 }
