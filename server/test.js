657908f0e5f4a6726534d1468169987864a313a7
Patrick Lorio <patrick@lorio.me>
2016-11-12 21:46:54 -0800

    Made progress on pizza integration

--- server/test.js
+++ server/test.js
@@ -1,3 +1,8 @@
 const pizza = require('./lib/pizza');
 
-pizza.buy_pizza('Patrick Lorio', 'patrick@lorio.me', '700 Clark Ave, St. Louis, MO, 63102', {}, ()=>{});
+pizza.buy_pizza('Patrick Lorio', 'patrick@lorio.me', 'cheese', {
+  city: 'Berkeley',
+  state: 'CA',
+  street: '1732 Highland Place',
+  zip_code: '94709'
+}, {}, (err)=>console.log(err));
